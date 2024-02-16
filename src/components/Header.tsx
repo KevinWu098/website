258cc7fd904e64239769df1cde6895b8549f535e
Kevin Wu <kevinwu098@gmail.com>
2024-02-16 12:27:39 -0800

    feat: hide nav on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,40 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
 import { ThemeToggle } from "./ThemeToggle";
 
 const Header = () => {
+    const [showNav, setShowNav] = useState(true);
+
+    useEffect(() => {
+        let prevScrollPos = window.pageYOffset;
+
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const scrollingUp = prevScrollPos > currentScrollPos;
+
+            setShowNav(scrollingUp);
+            prevScrollPos = currentScrollPos;
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <>
-            <nav className="wrapper absolute top-0 flex-between inset-x-0 z-30 h-24 bg-transparent">
+            <nav
+                className={cn(
+                    "wrapper top-0 fixed flex-between inset-x-0 z-30 h-24 w-full bg-transparent",
+                    showNav ? "flex" : "opacity-0 pointer-events-none",
+                    "transition-all duration-500",
+                )}
+            >
                 <div>{/* <ThemeToggle /> */}</div>
 
                 <ul className="flex space-x-10 font-bold text-lg">
