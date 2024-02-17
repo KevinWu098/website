c1a3459ed051f6c0402e09bbf0eb3c2d95331a8f
Kevin Wu <kevinwu098@gmail.com>
2024-02-16 22:41:04 -0800

    fix: reenable animations

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -14,10 +14,10 @@ const Hero = () => {
                     <div className="flex flex-col">
                         <span
                             className="font-bold text-xl lg:text-2xl"
-                            // data-aos="fade-up"
-                            // data-aos-duration="1000"
-                            // data-aos-delay="300"
-                            // data-aos-once="true"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="300"
+                            data-aos-once="true"
                         >
                             Hey! I'm
                         </span>
@@ -26,19 +26,19 @@ const Hero = () => {
                                 "font-bold bg-green-600 bg-blend-overlay inline-block text-transparent bg-clip-text",
                                 "text-9xl lg:text-[10rem] xl:text-[12rem] 2xl:text-[14rem] leading-none",
                             )}
-                            // data-aos="fade-up"
-                            // data-aos-duration="1000"
-                            // data-aos-delay="400"
-                            // data-aos-once="true"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="400"
+                            data-aos-once="true"
                         >
                             Kevin Wu
                         </h1>
                         <span
                             className="text-2xl lg:text-4xl font-bold flex items-center space-x-4"
-                            // data-aos="fade-up"
-                            // data-aos-duration="1000"
-                            // data-aos-delay="600"
-                            // data-aos-once="true"
+                            data-aos="fade-up"
+                            data-aos-duration="1000"
+                            data-aos-delay="600"
+                            data-aos-once="true"
                         >
                             <span>Fullstack Developer</span>
                             <Separator
