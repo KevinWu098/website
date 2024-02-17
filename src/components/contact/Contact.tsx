28fe3eaee449e8673623410b075674640eca4e2a
Kevin Wu <kevinwu098@gmail.com>
2024-02-16 17:09:25 -0800

    feat: add linkedin, email

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,15 +1,37 @@
-import { SiLinkedin } from "react-icons/si";
+import Link from "next/link";
+import { FaEnvelope, FaLinkedin } from "react-icons/fa";
+
+import { Separator } from "../ui/separator";
 
 const Contact = () => {
     return (
         <div className="bg-[#191919]">
-            <div className="wrapper pt-32">
-                <div className="text-7xl font-bold">Let's Chat!</div>
+            <div className="wrapper py-24 space-y-16">
+                <div className="space-y-3">
+                    <div className="text-7xl font-bold">let's chat!</div>
+                    <Separator className="w-36 h-1 bg-primary" />
+                </div>
 
-                <div>
-                    <div className="bg-slate-300 rounded-lg w-10 h-10 flex-center">
-                        <SiLinkedin className="w-8 h-8 text-black bg-blend-lighten" />
-                    </div>
+                <div className="flex justify-start space-x-12">
+                    <Link
+                        href="https://linkedin.com/in/kevinwu098"
+                        referrerPolicy="no-referrer"
+                        target="_blank"
+                        className="flex-center space-x-3 hover:scale-105"
+                    >
+                        <FaLinkedin className="w-8 h-8 text-slate-300 bg-blend-lighten" />
+                        <p className="text-4xl font-bold">LinkedIn</p>
+                    </Link>
+
+                    <Link
+                        href="mailto:kevinwu098@gmail.com"
+                        referrerPolicy="no-referrer"
+                        target="_blank"
+                        className="flex-center space-x-3 hover:scale-105"
+                    >
+                        <FaEnvelope className="w-7 h-7 text-slate-300 bg-blend-lighten" />
+                        <p className="text-4xl font-bold">Email</p>
+                    </Link>
                 </div>
             </div>
         </div>
